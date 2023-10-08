96a1980cbf8e4c00fbd1f18148a84204f411d798
cheininger <christoph_heininger@hotmail.com>
2023-10-08 13:12:40 +0200

    completed exercise (with solution)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const editedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  return editedString.split("").reverse().join("") == editedString;
 };
 
 // Do not edit below this line
