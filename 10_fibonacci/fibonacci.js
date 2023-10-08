25b796056a1ef9c5b9a441f6506497af96adf399
cheininger <christoph_heininger@hotmail.com>
2023-10-09 00:01:36 +0200

    completed exercise (with solution)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (nthNumber) {
+  if (nthNumber < 0) return "OOPS";
+  if (nthNumber === 0) return 0;
 
+  let firstNum = 1;
+  let secondNum = 0;
+
+  for (i = 2; i <= nthNumber; i++) {
+    let current = firstNum + secondNum;
+    secondNum = firstNum;
+    firstNum = current;
+  }
+
+  return firstNum;
 };
 
 // Do not edit below this line
